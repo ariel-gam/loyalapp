715eee88b52d0a3b2acffa9e5273317001e6a688
ariel-gam <lemoncripto@gmail.com>
2025-12-29 15:50:08 -0300

    fix: optimize middleware matcher to reduce invocations

--- src/middleware.ts
+++ src/middleware.ts
@@ -64,15 +64,9 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - manifest.json (PWA manifest)
-         * - sw.js (Service Worker)
-         * - images folders etc
-         */
-        '/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|workbox-.*|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    ],
+        '/admin/:path*',
+        '/setup/:path*',
+        '/login',
+        '/auth/:path*'
+    ]
 }
